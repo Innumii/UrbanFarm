02904910211bfa97f27d330b113a715f4432c957
phosphurous <122954378+phosphurous@users.noreply.github.com>
2024-03-21 17:40:23 +0800

    newProject

--- app/src/main/java/com/example/cs205_assignment4/MainActivity.java
+++ app/src/main/java/com/example/cs205_assignment4/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.cs205_assignment4;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,10 +19,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button btnClickMe = findViewById(R.id.buttonClick);
+        btnClickMe.setOnClickListener(view -> {
+            showAlertDialog("You get scammed!");
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    private void showAlertDialog(String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("AUGGIE")
+                .setMessage(message)
+                .setPositiveButton("KK, im a veggie", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create();
+        alertDialog.show();
+    }
+
 }
