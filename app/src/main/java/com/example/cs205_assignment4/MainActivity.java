e1b604782d0c948f9e60d839ad85c5dc4c7221bf
phosphurous <122954378+phosphurous@users.noreply.github.com>
2024-03-26 10:57:23 +0800

    fixed a problem where shape was not enlarging per frame.

--- app/src/main/java/com/example/cs205_assignment4/MainActivity.java
+++ app/src/main/java/com/example/cs205_assignment4/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
@@ -17,11 +19,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView squareView;
+    private AnimatedSquareView squareView;
     private TextView squareView2;
     private Handler handler;
     private int currentSize;
-    private int currentSize2;
+//    private int currentSize2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,68 +32,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         squareView = findViewById(R.id.squareView);
-        squareView2 = findViewById(R.id.squareView2);
+//        squareView2 = findViewById(R.id.squareView2);
         handler = new Handler(Looper.getMainLooper());
 
-        // Start increasing size every 5 seconds
-        currentSize = squareView.getWidth(); // Assuming square
-        startIncreasingSize();
-
-        currentSize2 = squareView2.getWidth(); // Assuming square
-        startIncreasingSize2();
-
-
-        Button btnClickMe = findViewById(R.id.buttonClick);
-        btnClickMe.setOnClickListener(view -> {
-            showAlertDialog("You get scammed!");
+        // Use ViewTreeObserver to wait for layout to be measured
+        ViewTreeObserver observer = squareView.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Start increasing size every 5 seconds
+                currentSize = squareView.getWidth(); // Get width after layout is measured
+                Log.d("TAG", "Message: " + currentSize);
+                squareView.startIncreasingSize();
+//                startIncreasingSize(squareView, currentSize);
+                squareView.getViewTreeObserver().removeOnGlobalLayoutListener(this); // Remove listener after use
+            }
         });
 
+//        ViewTreeObserver observer2 = squareView2.getViewTreeObserver();
+//        observer2.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @Override
+//            public void onGlobalLayout() {
+//                currentSize2 = squareView2.getWidth(); // Get width after layout is measured for squareView2
+//                startIncreasingSize(squareView2, currentSize2);
+//                squareView2.getViewTreeObserver().removeOnGlobalLayoutListener(this); // Remove listener after use
+//            }
+//        });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
 
-    private void startIncreasingSize() {
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (currentSize < 500) {
-                    currentSize += 10; // Increase size by 10 units
-                    squareView.setWidth(currentSize);
-                    squareView.setHeight(currentSize);
-                    startIncreasingSize(); // Call recursively for next iteration
-                }
-            }
-        }, 5000); // 5000 milliseconds = 5 seconds
-    }
-    private void startIncreasingSize2() {
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (currentSize2 < 500) {
-                    currentSize2 += 10; // Increase size by 10 units
-                    squareView2.setWidth(currentSize2);
-                    squareView2.setHeight(currentSize2);
-                    startIncreasingSize2(); // Call recursively for next iteration
-                }
-            }
-        }, 5000); // 5000 milliseconds = 5 seconds
+//
+//        Button btnClickMe = findViewById(R.id.buttonClick);
+//        btnClickMe.setOnClickListener(view -> {
+//            showAlertDialog("You get scammed!");
+//        });
+//
+//
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 
-    private void showAlertDialog(String message) {
-        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
-                .setTitle("AUGGIE")
-                .setMessage(message)
-                .setPositiveButton("KK, im a veggie", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                }).create();
-        alertDialog.show();
-    }
+//    private void showAlertDialog(String message) {
+//        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+//                .setTitle("AUGGIE")
+//                .setMessage(message)
+//                .setPositiveButton("KK, im a veggie", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                }).create();
+//        alertDialog.show();
+//    }
 
 }
